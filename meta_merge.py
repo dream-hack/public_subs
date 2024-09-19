a0f5a9f011f334ee8cde072a16a4682c96950126
DREAM_HACKER <dream-hacker@outlook.com>
2024-09-19 20:57:18 +0800

    update

--- meta_merge.py
+++ meta_merge.py
@@ -43,13 +43,13 @@ def get_physical_location(address):
 
     try:
         reader = geoip2.database.Reader(
-            "GeoLite2-City.mmdb"
+#           "GeoLite2-City.mmdb"
+            "https://github.com/P3TERX/GeoLite.mmdb/releases/download/2024.09.19/GeoLite2-City.mmdb"
         )  # 这里的路径需要指向你自己的数据库文件
         response = reader.city(ip_address)
         country = response.country.name
         city = response.city.name
         return f"{country}_{city}"
-        # return f"油管绵阿羊_{country}"
     except geoip2.errors.AddressNotFoundError as e:
         print(f"Error: {e}")
         return "Unknown"
